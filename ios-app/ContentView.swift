24ad7ccf30b6200b0dd3fa115388402c68ff82d0
JojoDom <adwoa_dadson@yahoo.com>
2023-04-16 19:54:46 +0000

    exploring padding,  and background

--- ios-app/ContentView.swift
+++ ios-app/ContentView.swift
@@ -12,39 +12,60 @@ struct ContentView: View {
         VStack(alignment: .leading, spacing: 70.0) {
             HStack(spacing : 20){
                 VStack{
-                   
                     Image("trans_fruits").resizable().aspectRatio(contentMode: .fit)
                     HStack {
+                        Spacer()
                         Text("Fruits")
                             .font(.body)
-                            .fontWeight(.bold)
+                            .fontWeight(.bold).foregroundColor(.black)
+                        Spacer()
                         Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                        Spacer()
                        
-                    }
-                }
+                    }.foregroundColor(.yellow).font(.caption)
+                }.padding(10)
+                .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
+                .padding(10).background(RoundedRectangle(cornerRadius: 15).fill(Color.green))
+
                 VStack{
                     Image("trans_veggies").resizable().aspectRatio(contentMode: .fit)
                     HStack{
-                        Text("Vegatables").font(.body).fontWeight(.bold)
-                    }
-                }
+                        Spacer()
+                        Text("Veg").font(.body).fontWeight(.bold).foregroundColor(.black)
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                        Spacer()
+                       
+                    }.foregroundColor(.yellow).font(.caption)
+                }.padding(10)
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
+                    .padding(10).background(RoundedRectangle(cornerRadius: 15).fill(Color.green))
             }.padding(10.0)
+                
+            
             HStack(spacing : 20){
                 VStack{
                     Image("trans_bakes").resizable().aspectRatio(contentMode: .fit)
                     Text("Bakes")
                         .font(.body)
                         .fontWeight(.bold)
-                }
+                }.padding(10)
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
+                    .padding(10).background(RoundedRectangle(cornerRadius: 15).fill(Color.green))
                 VStack{
                     Image("trans_diary").resizable().aspectRatio(contentMode: .fit)
                     Text("Diary")
                         .font(.body)
                         .fontWeight(.bold)
-                }
+                }.padding(10)
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.white))
+                    .padding(10).background(RoundedRectangle(cornerRadius: 15).fill(Color.green))
                 
                 
-            }
+            }.padding(10.0)
             
             
         }
