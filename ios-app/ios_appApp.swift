2fbf26428f95a63114fa2ce50e7d494163eefef2
JojoDom <adwoa_dadson@yahoo.com>
2023-04-16 12:28:34 +0000

    Initial Commit

---
+++ ios-app/ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_appApp.swift
+//  ios-app
+//
+//  Created by Adwoa Dadson on 16/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
