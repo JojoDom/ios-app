28b054caea51f4a503dbd13d242ce44d912b2b55
JojoDom <adwoa_dadson@yahoo.com>
2023-04-16 15:52:47 +0000

    exploring layouts in swift ui

--- ios-app/ContentView.swift
+++ ios-app/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Swift")
+        VStack(alignment: .leading, spacing: 70.0) {
+            HStack(spacing : 20){
+                VStack{
+                   
+                    Image("trans_fruits").resizable().aspectRatio(contentMode: .fit)
+                    HStack {
+                        Text("Fruits")
+                            .font(.body)
+                            .fontWeight(.bold)
+                        Image(systemName: "star.fill")
+                       
+                    }
+                }
+                VStack{
+                    Image("trans_veggies").resizable().aspectRatio(contentMode: .fit)
+                    HStack{
+                        Text("Vegatables").font(.body).fontWeight(.bold)
+                    }
+                }
+            }.padding(10.0)
+            HStack(spacing : 20){
+                VStack{
+                    Image("trans_bakes").resizable().aspectRatio(contentMode: .fit)
+                    Text("Bakes")
+                        .font(.body)
+                        .fontWeight(.bold)
+                }
+                VStack{
+                    Image("trans_diary").resizable().aspectRatio(contentMode: .fit)
+                    Text("Diary")
+                        .font(.body)
+                        .fontWeight(.bold)
+                }
+                
+                
+            }
+            
+            
         }
-        .padding()
+        
+        
+
     }
 }
 
